d4b65c501bbb0b9a0344c360b83df69b8d0f4f39
Peter Hearne <hearne73@gmail.com>
2015-10-12 23:15:13 +0100

    Initial commit

---
+++ src/imageProcessing/ConnectedComponentImage.java
@@ -0,0 +1,76 @@
+package imageProcessing;
+
+
+import edu.princeton.cs.introcs.Picture;
+
+/*************************************************************************
+ * Compilation: javac ConnectedComponentImage.java
+ *
+ * The <tt>ConnectedComponentImage</tt> class
+ * <p>
+ * You do the rest....
+ *
+ * @author Peter Hearne
+ *
+ *************************************************************************/
+public class ConnectedComponentImage {
+
+    Picture picture;
+
+    /**
+     * Initialise fields
+     *
+     * @param fileLocation
+     */
+    public ConnectedComponentImage(String fileLocation) {
+        // TODO
+    }
+
+    /**
+     * Returns the number of components identified in the image.
+     *
+     * @return the number of components (between 1 and N)
+     */
+    public int countComponents() {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Returns the original image with each object bounded by a red box.
+     *
+     * @return a picture object with all components surrounded by a red box
+     */
+    public Picture identifyComonentImage() {
+
+        return null;
+
+    }
+
+    /**
+     * Returns a picture with each object updated to a random colour.
+     *
+     * @return a picture object with all components coloured.
+     */
+    public Picture colourComponentImage() {
+
+        return null;
+
+    }
+
+    public Picture getPicture() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Returns a binarised version of the original image
+     *
+     * @return a picture object with all components surrounded by a red box
+     */
+    public Picture binaryComponentImage() {
+
+        return null;
+    }
+
+}
---
+++ test/imageProcessing/ConnectedComponentImageTest.java
@@ -0,0 +1,48 @@
+package imageProcessing;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Owner on 10/12/2015.
+ */
+public class ConnectedComponentImageTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testCountComponents() throws Exception {
+
+    }
+
+    @Test
+    public void testIdentifyComonentImage() throws Exception {
+
+    }
+
+    @Test
+    public void testColourComponentImage() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPicture() throws Exception {
+
+    }
+
+    @Test
+    public void testBinaryComponentImage() throws Exception {
+
+    }
+}
